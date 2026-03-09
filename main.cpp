2dbd162499060d7f20591ce3af2388c20d419c2f
t <tristanandkauai@icloud.com>
2026-03-08 19:16:32 -0700

    Initial push of necessary files required by the assignment. IsFloat implementation provided by Professor Holliday. Implamentation can be found on his activeprofessor website at https://sites.google.com/a/fullerton.edu/activeprofessor/4-subjects/x86-programming/library-software/isfloat-x86-only

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std:
+
+extern "C"{
+
+}
+
+int main() {
+    cout<<"Welcome to Arrays of floating point numbers." << endl;
+    cout<<"Brought to you by Tritan Chen" << endl;
+
+}
