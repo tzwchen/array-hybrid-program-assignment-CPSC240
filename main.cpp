e63a1e19648a7acd92d6f7db2758f9cd7f2f164a
t <tristanandkauai@icloud.com>
2026-03-08 20:50:52 -0700

    implemented main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <iostream>
+
 using namespace std;
 
-extern "C"{
-
-}
+extern "C" long manager(); // Declare the assembly function
 
 int main() {
-    cout<<"Welcome to Arrays of floating point numbers." << endl;
-    cout<<"Brought to you by Tritan Chen" << endl;
+    cout << "Welcome to Arrays of floating point numbers." << endl;
+    cout << "Brought to you by Tristan Chen" << endl;
 
+    long count = manager(); // Call the manager and store the return value
+
+    cout << "The main function received " << count << " and will now keep it." << endl;
+    cout << "A zero will be returned to the operating system. Bye." << endl;
+
+    return 0;
 }
